5ca24cd45b22d9291dae675c83935df51d20b8f2
nandan95 <nandankango95@gmail.com>
2024-12-11 12:38:58 +0530

    drag -drop functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-
+import { useState } from 'react';
+import Notes from './components/Notes';
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "Link in bio for my Frontend Interview Prep Course",
+    },
+    {
+      id: 2,
+      text: "Like this Video and Subscribe to Roadside Coder",
+    },
+  ]);
+
+  const [note, setNote] = useState("");
   return (
-    <div className="App">
-      
-    </div>
+      <Notes notes={notes} setNotes={setNotes}/>
   );
 }
 
